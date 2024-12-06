094dde0013521a6a5e15299b3bf843333bbb8b63
panchalchetan618 <panchalchetan315@gmail.com>
2024-12-07 00:57:19 +0530

    Hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/sections/hero";
 import About from "@/components/sections/about";
 import Skills from "@/components/sections/skills";
-import Portfolio from "@/components/sections/portfolio";
+// import Portfolio from "@/components/sections/portfolio";
 import Contact from "@/components/sections/contact";
 import Footer from "@/components/sections/footer";
 
